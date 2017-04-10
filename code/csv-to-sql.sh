032c0a0300b5ad4a561a9c652a3ab50e43314c9a
gaba <gabelula@gmail.com>
2017-04-10 07:32:02 -0700

    More data

--- code/csv-to-sql.sh
+++ code/csv-to-sql.sh
@@ -3,5 +3,7 @@ FILES=$(ls | grep csv)
 for f in $FILES
   do
      echo "Transforming $f"
-     csvsql $f >> ../create.sql
+     t=$(echo $f | tr -d ".csv" | tr -d "-")
+     csvsql -i postgresql $f >> ../create.sql
+     csvsql --db postgresql:///causa --table $t --insert $f
  done
