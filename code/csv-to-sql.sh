86734a20d88600ee8a6fd16dc8db1c485147fac0
gaba <gabelula@gmail.com>
2017-06-05 19:44:13 -0700

    Move code

--- code/csv-to-sql.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-FILES=$(ls | grep csv)
-for f in $FILES
-  do
-     t=$(echo $f | sed "s/\.csv//" | tr -d "-" | tr -d "_")
-     echo "Transforming $f into $t"
-     # csvsql -i postgresql --tables $t $f >> ./create.sql
-     csvsql --db postgresql:///causa_natura --table $t --insert $f
- done
